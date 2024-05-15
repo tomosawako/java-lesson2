9df806c6bd5fae8e361b24994c55d06b9b5cd261
EC2 Default User <ec2-user@ip-172-31-33-88.ap-northeast-1.compute.internal>
2024-05-15 05:39:12 +0000

    [Add]JavaLesson

---
+++ 2-10Q/LoopMessage.java
@@ -0,0 +1,10 @@
+class LoopMessage{
+  public static void main(String[] args){
+    //メッセージを10回表示(while文)
+    int i = 1;
+    while( i <= 10 ){
+      System.out.println( i + "回目:フリーラーニングを続けたい!");
+      i++;
+    }
+  }
+}
