1eaf28165df4f896c2606d50a40191f88e077b2b
EC2 Default User <ec2-user@ip-172-31-33-88.ap-northeast-1.compute.internal>
2024-05-15 01:32:19 +0000

    [Add]JavaLesson2

---
+++ Branch2.java
@@ -0,0 +1,19 @@
+class Branch2{
+    public static void main(String[] args){
+        int price = Integer.parseInt(args[0]);
+        double rate = 0.10;
+        int discount, amount;
+        
+        if( price >= 5000) {
+            discount = 500;
+        } else if( price >= 3000) {
+            discount = 300;
+        } else {
+            discount = 0;
+        }
+        
+        amount = (int)((price - discount) * (1 + rate));
+        System.out.println("値引き金額:" + discount + "円");
+        System.out.println("税込価格:" + amount + "円");
+    }
+}
---
+++ Loop3.java
@@ -0,0 +1,15 @@
+class Loop3{
+    public static void main(String[] args) {
+        int[] score = new int[3];
+        score[0] = 80;
+        score[1] = 100;
+        score[2] = 75;
+        
+        String[] name = {"菅原", "村山", "寺島"};
+        
+        for(int i = 0; i < score.length; i++){
+            System.out.println(name[i] + "さん" + score[i] + "点");
+        }
+        System.out.println("受験者数：" + score.length + "名");
+    }
+}
